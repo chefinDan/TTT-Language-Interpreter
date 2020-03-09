118939096ccf878dfed03ac46a860360ad767171
mpandrews <46432743+mpandrews@users.noreply.github.com>
2020-03-09 13:37:38 -0700

    Temporarily stashing tests.

--- refactor/Tests.hs
+++ refactor/Tests.hs
@@ -16,9 +16,19 @@ import RunLibrary
 --   55
 --
 --   >>> eval (ArithExp (Add (Lit (I 5)) (Lit (S "foo")))) emptyContext
---   (fromList [],Error "Invalid operands to add: Operands are of non-addable types.") 
-<<<<<<< HEAD
+--   (fromList [],Err (E (BadOperands "Add") []))
 --
---   >>> eval (Arith) 
-=======
->>>>>>> 498c7c587844f382fdf62f80b4b622cdb23c2901
+--   >>> eval (ArithExp (Divide (Lit (I 5)) (Lit (I 2)))) emptyContext
+--   (fromList [],Valid (I 2))
+--
+--   >>> eval (ArithExp (Divide (Lit (S "foofoofoofoo")) (Lit (I 2)))) emptyContext
+--   (fromList [],Valid (S "foofoo"))
+--
+--   >>> eval (ArithExp (Divide (Lit (I 10)) (Lit (I 0)))) emptyContext
+--   (fromList [],Err (E DivideByZero []))
+--
+--   >>> eval (ArithExp (Divide (Lit (I 10)) (Lit (S "foo")))) emptyContext
+--   (fromList [],Err (E (BadOperands "divide") []))
+--
+--   >>> eval (ArithExp (Divide (Lit (S "foo")) (Lit (S "bar"))) emptyContext
+--   (fromList [],Err (E (BadOperands "divide") []))
